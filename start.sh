8df60ab84a4a76b0737662263d552d2a81d3a4f7
Mihai <qqqromaniamicrovolts@gmail.com>
2026-07-12 22:07:07 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# Start FastAPI backend
-python api.py &
+set -e
 
-# Serve React frontend on port 3000
-python -m http.server 3000 --directory frontend/build &
-
-# Wait for any process to exit
-wait -n
-
-# Exit with status of process that exited first
-exit $?
+exec python api.py
